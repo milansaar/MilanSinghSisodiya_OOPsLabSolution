44a5d567d8bec5950f189074139fd452dbe4de5e
Milan Singh Sisodiya <milansinghsisodiya2@gmail.com>
2022-11-16 18:19:26 +0530

    Add files via upload

---
+++ Email Application/src/com/mail/service/CredentialService.java
@@ -0,0 +1,44 @@
+package com.mail.service;
+
+import java.util.Random;
+
+import com.mail.model.Employee;
+
+public class CredentialService {
+
+	public char[] generatePassword() {
+
+		String capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String smallLetters = "abcdefghijklmnopqrstuvwxyz";
+		String numbers = "0123456789";
+		String specialCharacters = "!@#$%^&*_=+-/.?<>)";
+
+		String values = capitalLetters + smallLetters + numbers + specialCharacters;
+	
+		// Using random method for random password generation
+		Random random = new Random();
+
+		char[] password = new char[8];
+
+		for (int i = 0; i < 8; i++) {
+			
+			password[i] = values.charAt(random.nextInt(values.length()));
+
+		}
+		return password;
+	}
+	
+	public String generateEmailAddress(String firstName, String lastName, String department)
+	{
+		return firstName+lastName+"@"+department+".abc.com";
+	}
+	
+	public void showCredentials(Employee employee, String email, char[] generatedPassword) {
+		System.out.println("Dear "+employee.getFirstName()+ " your generated credentials are as follows ");
+		System.out.println("Email --> "+ email);
+		System.out.print("Password --> 1"
+				+ "");
+		System.out.print(generatedPassword);
+	}
+	
+}
